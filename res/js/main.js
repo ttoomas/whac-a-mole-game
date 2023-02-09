81f44563f099f647f52b852bc3a7cad8f1b37002
Tomáš Folprecht <tomas.folprecht@student.spsmb.cz>
2023-02-09 15:13:57 +0100

    small updates, max score, localstorage

--- res/js/main.js
+++ res/js/main.js
@@ -1,12 +1,41 @@
 const holeBxs = document.querySelectorAll(".game__holeBx");
 const holes = document.querySelectorAll(".game__hole");
 const scoreText = document.querySelector(".game__score");
+const timeText = document.querySelector(".game__time");
+const currentScoreText = document.querySelector('.game__currentScore');
+const maxScoreText = document.querySelector('.game__maxScore');
 const hammer = document.querySelector('.hammer__bx');
+const startBtn = document.querySelector('.game__startBtn');
+const startBx = document.querySelector('.game__startBx');
 
 let currentRandomHole;
 let score = 0;
+let maxScore = 0;
+let initialTime = 4;
+let time = initialTime;
 
 
+window.addEventListener('load', () => {
+    let loadedMaxScore = localStorage.getItem("holeMaxScore");
+
+    if(loadedMaxScore !== null && loadedMaxScore !== 0){
+        maxScore = loadedMaxScore;
+
+        maxScoreText.innerText = maxScore;
+    }
+})
+
+
+
+startBtn.addEventListener('click', () => {
+    startBx.style.display = "none";
+    hammer.style.display = "block";
+
+    document.body.style.cursor = "none";
+    
+    gameLoop();
+    timeLoop();
+})
 
 window.addEventListener('mousedown', () => {
     hammer.animate(
@@ -52,7 +81,8 @@ function randomNum(max){
 }
 
 
-let gameLoopInterval;
+let gameLoopInterval,
+    timeLoopInterval;
 
 function gameLoop(){
     gameLoopInterval = setInterval(() => {
@@ -64,7 +94,45 @@ function gameLoop(){
     }, 1000);
 }
 
-gameLoop();
+function timeLoop(){
+    timeLoopInterval = setInterval(() => {
+        time--;
+
+        timeText.innerText = time;
+
+        if(time <= 0){
+            endGame();
+        }
+    }, 1000);
+}
+
+function endGame(){
+    clearInterval(timeLoopInterval);
+    clearInterval(gameLoopInterval);
+
+    if(currentRandomHole !== undefined) holes[currentRandomHole].style.display = "none";
+
+    currentScoreText.innerText = score;
+
+    if(maxScore < score){
+        maxScore = score;
+    
+        maxScoreText.innerText = maxScore;
+        
+        localStorage.setItem("holeMaxScore", maxScore);
+    }
+
+    startBx.style.display = "flex";
+    hammer.style.display = "none";
+
+    document.body.style.cursor = "auto";
+
+    time = initialTime;
+    timeText.innerText = time;
+
+    score = 0;
+    scoreText.innerText = score;
+}
 
 
 let mouseX,
