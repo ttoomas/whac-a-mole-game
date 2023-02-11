96654c40f324cd3ba804cbf10319e3918d096eab
ttoomas <folprecht.to@gmail.com>
2023-02-11 15:13:32 +0100

    create game fields in js, points, event listener

--- res/js/main.js
+++ res/js/main.js
@@ -1,6 +1,30 @@
-const gameFields = document.querySelectorAll('.game__field');
+const gameFieldBx = document.querySelector('.game__fields');
+
+let gameFields = [];
 
 let activeFields = [];
+let points = 0;
+
+let animalTypes = [
+    {
+        id: 1,
+        pointValue: 10
+    },
+    {
+        id: 2,
+        pointValue: 7
+    },
+    {
+        id: 3,
+        pointValue: 1
+    },
+    {
+        id: 4,
+        pointValue: -20
+    }
+]
+
+createGameFields(9);
 
 
 class Field{
@@ -14,7 +38,10 @@ class Field{
     }
 
     randomField(){
-        if(this.currentField !== null) gameFields[this.currentField].style.backgroundImage = null;
+        if(this.currentField !== null){
+            gameFields[this.currentField].style.backgroundImage = null;
+            gameFields[this.currentField].removeAttribute('data-animal-id');
+        }
 
         do {
             this.currentField = randomNumber(0, 8);
@@ -27,9 +54,12 @@ class Field{
         activeFields.push(this.currentField);
 
 
-        this.currentAnimal = randomNumber(1, 4);
+        do {
+            this.currentAnimal = randomNumber(1, 4);
+        } while (this.currentAnimal === 4 && points < (animalTypes[3].pointValue * -1))
 
         gameFields[this.currentField].style.backgroundImage = `url(./res/images/animal${this.currentAnimal}.png)`;
+        gameFields[this.currentField].setAttribute('data-animal-id', this.currentAnimal);
 
         setTimeout(() => {
             this.randomField();
@@ -37,14 +67,51 @@ class Field{
     }
 }
 
-const firstField = new Field(600, 1000);
+const firstField = new Field(500, 1000);
 firstField.randomField();
 
-const secondField = new Field(200, 600);
-secondField.randomField();
+// const secondField = new Field(200, 600);
+// secondField.randomField();
 
 
 
+class EventListener{
+    fieldOnClick(){
+        window.addEventListener('click', (e) => {
+            if(!e.target.classList.contains('game__field') || !e.target.style.backgroundImage) return;
+
+            let animalId = e.target.getAttribute('data-animal-id');
+            let animalStats = animalTypes[animalId - 1];
+
+            points += animalStats.pointValue;
+
+            e.target.style.backgroundImage = null;
+
+            console.log(points);
+        })
+    }
+}
+
+const eventListener = new EventListener();
+eventListener.fieldOnClick();
+
+
+
+// Helpers
 function randomNumber(min, max){
     return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function createGameFields(count){
+    gameFields = [];
+
+    gameFieldBx.innerText = '';
+
+    for (let i = 0; i < count; i++) {
+        let newGameField = document.createElement('div');
+        newGameField.classList.add('game__field');
+
+        gameFieldBx.appendChild(newGameField);
+        gameFields.push(newGameField);
+    }
 }
